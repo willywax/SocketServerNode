34acaabd268188b21e2ad4f0474d4dad5dae54b2
Willywax <williamtrans2011@.com>
2022-03-20 22:22:27 +0300

    update port

--- serverTest.js
+++ serverTest.js
@@ -5,7 +5,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const port = 9012;
+const port = process.env.PORT || 9012;
 
 const socketLists = [];
 
